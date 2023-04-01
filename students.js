9d5b78d5a020f82624e569bf9436ff06dd12c46f
abulicious1 <abunanga1990@gmail.com>
2023-04-01 15:49:29 +0000

    Done with the table display

--- students.js
+++ students.js
@@ -1,4 +1,6 @@
 let students =[]
+loadData()
+
 
 function add(){
     let full_name = document.getElementById('full_name').value;
@@ -15,7 +17,33 @@ function add(){
 
     students.push(student)
 
-    localStorage.setItem('students',students)
+    localStorage.setItem('students',JSON.stringify(students) )
 
     console.log(students)
+
+    loadData()
+}
+
+function loadData(){
+    let table = document.getElementById('body');
+    let students_data = localStorage.getItem('students');
+
+    table.innerHTML=' '
+    students=JSON.parse(students_data)
+
+    for (let i=0;i<students.length;i++){
+        let tr=table.insertRow(i);
+        let cell1 = tr.insertCell(0)
+        let cell2 = tr.insertCell(1)
+        let cell3 = tr.insertCell(2)
+        let cell4 = tr.insertCell(3)
+
+        cell1.innerHTML= students[i].full_name
+        cell2.innerHTML= students[i].age
+        cell3.innerHTML= students[i].gender
+        cell4.innerHTML= students[i].phone
+        // console.log(students[i]);
+    }
+
+    console.log(JSON.parse(students_data))
 }
